48c30e828fab11578ada082a207c0c02f20646ab
凤之兮原 <1577310596@qq.com>
2022-10-13 23:15:19 +0800

    choer: add VForm and finish config

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,10 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'uno.css'
 import './assets/css/style.css'
+import VForm3 from 'vform3-builds'
 
 import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
-app.use(router)
-app.use(ElementPlus)
-
-app.mount('#app')
+app.use(router).use(ElementPlus).use(VForm3).mount('#app')
