ba9ce5c4106c297cc0678d802915c19c4de9e410
凤之兮原 <1577310596@qq.com>
2022-10-13 11:17:44 +0800

    feat: add Home.vue

---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- 头部 -->
+  <Header />
+  <!-- 轮播图 -->
+  <Carousel />
+  <!-- 主体 -->
+  <!-- <InputForm /> -->
+</template>
+
+<script lang="ts" setup>
+import Header from '@/views/home/Header.vue';
+import Carousel from '@/views/home/Carousel.vue'
+// import InputForm from '@/views/InputForm.vue';
+</script>
