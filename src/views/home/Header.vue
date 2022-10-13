2b75a88e6acf54e3823f4de2fb3dc64c2d40bc0b
凤之兮原 <1577310596@qq.com>
2022-10-13 11:17:24 +0800

    extract path

---
+++ src/views/home/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header p-10 flex="~" justify-between items-center dark:bg-gray-800 dark:text-gray-100>
+    <div>
+      <div flex="~" items-center>
+        <div i-logos:vue mr-10 />
+        <span text-sm>信息查询平台</span>
+      </div>
+    </div>
+    <div @click="toggle()">
+      <div i-bi:moon-stars dark:i-bxs:sun cursor-pointer />
+    </div>
+  </header>
+</template>
+
+<script setup lang='ts'>
+import { useDark, useToggle } from '@vueuse/core'
+const isDark = useDark()
+const toggle = useToggle(isDark)
+</script>
+
+<style scoped>
+
+</style>
