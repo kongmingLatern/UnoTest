f9e8f721359198835767c0350e248be92239d38b
凤之兮原 <1577310596@qq.com>
2022-10-14 22:53:33 +0800

    feat: add Back.vue

---
+++ src/pages/Back.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- 头部 -->
+  <Header />
+  <!-- 评论 -->
+  <Card />
+</template>
+
+<script lang="ts" setup>
+import Header from '@/views/back/Header.vue';
+import Card from '@/views/home/Card.vue'
+</script>
