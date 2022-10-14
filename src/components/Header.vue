2ba71d7b235b0c6e49c38210193a3816fcca65be
凤之兮原 <1577310596@qq.com>
2022-10-14 23:16:43 +0800

    move: move Header to components

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header p-10 f-b-c dark:bg-gray-800 dark:text-gray-100>
+    <div>
+      <div flex="~" a-i-c>
+        <div i-logos:vue mr-10 />
+        <span text-lg>{{title}}</span>
+      </div>
+    </div>
+    <div flex a-i-c>
+      <div i-bi:moon-stars dark:i-bxs:sun cursor-pointer @click="toggle()" />
+      <el-button ml-5 plain sm:text-sm lg:text-lg @click="$router.push(`${route}`)">
+        {{text}}
+      </el-button>
+    </div>
+  </header>
+</template>
+
+<script setup lang='ts'>
+import { useDark, useToggle } from '@vueuse/core'
+const props = defineProps<{
+  title?: string,
+  text?: string,
+  route?: string
+}>()
+const isDark = useDark()
+const toggle = useToggle(isDark)
+</script>
+
+<style scoped>
+
+</style>
